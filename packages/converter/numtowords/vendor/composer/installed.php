59757b2f2897ce74cfb4266623aa923513d2ae6e
techswarn <tech.swarns@gmail.com>
2023-03-18 12:36:52 +0530

    code update

---
+++ packages/converter/numtowords/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '89337450cc54782ac68c619a7ee7732b673183e4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '89337450cc54782ac68c619a7ee7732b673183e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nineteenfeet/nf-number-to-word' => array(
+            'pretty_version' => '1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => '639a6071746b6c928a01e915958e5b987c898475',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nineteenfeet/nf-number-to-word',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
